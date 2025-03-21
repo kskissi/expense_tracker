510b211bad4114be576739965358ce8e0fca6bf7
“kskissi” <kwakusarfokissi.ksk@gmail.com>
2025-03-21 16:43:39 -0700

    app

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def index():
     return render_template('index.html', expenses=expenses, total_expenses=total_expenses)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8081) # The port has been changed to 8081.
+    app.run(debug=True, host='0.0.0.0', port=8081) # Modified to use host='0.0.0.0'
